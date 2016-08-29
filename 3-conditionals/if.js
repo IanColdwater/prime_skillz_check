01c9207624ccc5d3820eae7044bb7df2a147a05c
Ian <shiny@what.local>
2016-08-29 15:04:29 -0500

    done

--- 3-conditionals/if.js
+++ 3-conditionals/if.js
@@ -4,10 +4,16 @@
 var first = 3;
 var second = 5;
 
+if (first == second){
+  console.log("Hello");
+}
+
 // 2 - Expand your answer above to
 // console log "Goodbye" if
 // the variables are not equal
-
+else if (first != second){
+  console.log("Goodbye");
+}
 // 3 - Write a statement that
 // assigns the value of the addition
 // of the first & second variables to a new
@@ -15,3 +21,9 @@ var second = 5;
 // than second. If second is greater than first
 // assign the difference of the first & second
 // variables to third.
+
+if (first > second){
+  var third = first + second;
+} else if (second > first){
+  var third = (second - first);
+}
